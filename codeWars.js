944387aeb31855c579566c3a2f0df10352bdc4c2
Nirweint <nirweint_1@mail.ru>
2021-10-07 21:58:19 +0300

    29 cw

--- codeWars.js
+++ codeWars.js
@@ -284,3 +284,17 @@ function feast(beast, dish) {
 feast("sdsdsd", "dsdsdsd")
 
 // 29
+function fakeBin(x){
+	let str = x.split('')
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] < 5) {
+			str[i] = '0'
+		} else {
+			str[i] = '1';
+		}
+	}
+	return str.join('')
+}
+
+fakeBin('45385593107843568')
+
